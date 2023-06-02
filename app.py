1b4c922f0c8b3294d1e2f7c63d6e4d3b22029a7a
Diomedes L. Potente <134224004+DiomPotente@users.noreply.github.com>
2023-06-03 00:10:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def app():
         st.text(df.shape)
 
         #Randomly select samples
-        label_0=df[df['label']==0].sample(n=14)
+        label_0=df[df['label']==0].sample(n=15)
         label_1=df[df['label']==1].sample(n=16)
 
         train = pd.concat([label_1, label_0])
